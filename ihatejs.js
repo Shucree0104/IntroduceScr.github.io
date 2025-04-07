dc88413e367f0e4508706243e666c75de2545ec4
슈크리북\jungc <Jungchangim22@hufs.ac.kr>
2025-04-07 23:22:01 +0900

    1트

---
+++ ihatejs.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("menu1").onclick = function () {
+      alert("준비중입니다. 슈크리가 뭔지 궁금하다면 그건 다음 시간에..");
+    };
+  
+    document.getElementById("menu2").onclick = function () {
+      alert("준비중입니다. 근데 아래에도 contact있는데 이건 왜만든걸까요");
+    };
+  
+    document.getElementById("menu3").onclick = function () {
+      alert("준비중입니다. 사실 아직 구현 능력이 안돼요..");
+    };
+  
+    document.getElementById("menu4").onclick = function () {
+      alert("준비중입니다. 약간 미술관 같은 기업 웹사이트를 만들고 싶었어요!");
+    };
+  });
